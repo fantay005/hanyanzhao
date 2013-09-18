084d3eb1dd564912012e97c8326899f497d6739c
XiQingping <xi.qingping@gmail.com>
2013-09-18 20:36:36 +0800

    添加7段数码管显示功能

---
+++ Led/seven_seg_led.c
@@ -0,0 +1,159 @@
+#include "string.h"
+#include "stm32f10x_gpio.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+
+static char __displayChar[10];
+static char __changed;
+static xSemaphoreHandle __semaphore = NULL;
+
+#define CHANNEL0_DATA_GPIO_PORT GPIOE
+#define CHANNEL0_DATA_GPIO_PIN  GPIO_Pin_2
+
+#define CHANNEL1_DATA_GPIO_PORT  GPIOF
+#define CHANNEL1_DATA_GPIO_PIN   GPIO_Pin_7
+
+#define CLK245_GPIO_PORT GPIOF
+#define CLK245_GPIO_PIN  GPIO_Pin_8
+
+#define LAUNCH_GPIO_PORT  GPIOE
+#define LAUNCH_GPIO_PIN   GPIO_Pin_6
+
+
+
+//用于显示T_T_H的I/O口--------------------------------
+static inline void  __channel0SetDataHigh(void) {
+	GPIO_SetBits(CHANNEL0_DATA_GPIO_PORT, CHANNEL0_DATA_GPIO_PIN);
+}
+
+static inline void __channel0SetDataLow(void) {
+	GPIO_ResetBits(CHANNEL0_DATA_GPIO_PORT, CHANNEL0_DATA_GPIO_PIN);
+}
+
+static inline void  __channel1SetDataHigh(void) {
+	GPIO_SetBits(CHANNEL1_DATA_GPIO_PORT, CHANNEL1_DATA_GPIO_PIN);
+}
+
+static inline void __channel1SetDataLow(void) {
+	GPIO_ResetBits(CHANNEL1_DATA_GPIO_PORT, CHANNEL1_DATA_GPIO_PIN);
+}
+
+static inline void __setClkHigh(void) {
+	GPIO_SetBits(CLK245_GPIO_PORT, CLK245_GPIO_PIN);
+}
+
+static inline void __setClkLow(void) {
+	GPIO_ResetBits(CLK245_GPIO_PORT, CLK245_GPIO_PIN);
+}
+
+static inline void __setLaunchHigh(void) {
+	GPIO_SetBits(LAUNCH_GPIO_PORT, LAUNCH_GPIO_PIN);
+}
+
+static inline void __setLaunchLow(void) {
+	GPIO_ResetBits(LAUNCH_GPIO_PORT, LAUNCH_GPIO_PIN);
+}
+
+void SevenSegLedInit(void) {
+	GPIO_InitTypeDef GPIO_InitStructure;
+	if (__semaphore != NULL) {
+		return;
+	}
+	
+	memset(__displayChar, 0xFF, sizeof(__displayChar));
+	__changed = 1;
+
+	vSemaphoreCreateBinary(__semaphore);
+
+	__channel0SetDataHigh();
+	__channel1SetDataHigh();
+	__setClkHigh();
+	__setLaunchHigh();
+
+	GPIO_InitStructure.GPIO_Pin = CHANNEL0_DATA_GPIO_PIN;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_Init(CHANNEL0_DATA_GPIO_PORT, &GPIO_InitStructure);
+
+	GPIO_InitStructure.GPIO_Pin = CHANNEL1_DATA_GPIO_PIN;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_Init(CHANNEL1_DATA_GPIO_PORT, &GPIO_InitStructure);
+
+	GPIO_InitStructure.GPIO_Pin = CLK245_GPIO_PIN;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_Init(CLK245_GPIO_PORT, &GPIO_InitStructure);
+
+	GPIO_InitStructure.GPIO_Pin = LAUNCH_GPIO_PIN;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_Init(LAUNCH_GPIO_PORT, &GPIO_InitStructure);
+}
+
+static char __charToDisplayContent(unsigned char c) {
+	static const unsigned char displayTable[] = { 0xBF, 0x0B, 0x77, 0x5F, 0xCB, 0xDD, 0xFD, 0x0F, 0xFF, 0xDF, 0x01, 0x41 };
+	if (c >= sizeof(displayTable)) {
+		return 0;
+	}
+	return displayTable[c];
+}
+
+int SevenSegLedSetContent(unsigned int index, char what) {
+	char content;
+
+	if (index >= sizeof(__displayChar)) {
+		return 0;
+	}
+
+	content = __charToDisplayContent(what);
+
+
+	xSemaphoreTake(__semaphore, portMAX_DELAY);
+	if (content != __displayChar[index]) {
+		__displayChar[index] = content;
+		__changed = 1;
+	}
+	xSemaphoreGive(__semaphore);
+
+	return 1;
+}
+
+static void __shiftByte(unsigned char c0, unsigned char c1) {
+	unsigned int bit;
+	for (bit = 0x01; bit != 0x100; bit = bit << 1) {
+		if (bit & c0) {
+			__channel0SetDataHigh();
+		} else {
+			__channel0SetDataLow();
+		}
+
+		if (bit & c1) {
+			__channel1SetDataHigh();
+		} else {
+			__channel1SetDataLow();
+		}
+
+		__setClkLow();
+		__setClkLow();
+		__setClkHigh();
+	}
+}
+
+void SevenSegLedDisplay(void) {
+	int i;
+	if (! __changed) {
+		return;
+	}
+
+	xSemaphoreTake(__semaphore, portMAX_DELAY);
+	for (i = 0; i < sizeof(__displayChar) / 2; ++i) {
+		__shiftByte(__displayChar[i], __displayChar[i + sizeof(__displayChar) / 2]);
+		__setLaunchLow();
+		__setLaunchLow();
+		__setLaunchLow();
+		__setLaunchHigh();
+	}
+	xSemaphoreGive(__semaphore);
+}
---
+++ Led/seven_seg_led.h
@@ -0,0 +1,8 @@
+#ifndef __SEVEN_SEG_LEG_HH__
+#define __SEVEN_SEG_LEG_HH__
+
+void SevenSegLedInit(void);
+void SevenSegLedSetContent(int index, char what);
+void SevenSegLedDisplay(void);
+
+#endif
