c5a95222174deeb684d3a3f1304d26b8481ff909
hanyanzhao <championhan@163.com>
2014-02-26 10:47:55 +0800

    同步时间函数出错修改

---
+++ Preincludes/speaker.h
@@ -0,0 +1,13 @@
+#ifndef __PREINCLUDE_H__
+#define __PREINCLUDE_H__
+
+#include "preinclude_commoh.h"
+
+//GPRS¿Æ∞»≈‰÷√
+#define __SPEAKER__                     1
+
+#define __TARGET_STRING__               "SPEAKER"
+
+#else
+#  error "Preinclude file can only be included once in command line with --preinclude=xxxx"
+#endif
