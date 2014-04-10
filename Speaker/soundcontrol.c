ad4e7a646579ba632e111f827a1747e27172354d
hanyanzhao <championhan@163.com>
2014-04-10 13:39:25 +0800

    恢复之前删除掉的部分代码

--- Speaker/soundcontrol.c
+++ Speaker/soundcontrol.c
@@ -26,6 +26,16 @@ static void initHardware(void) {
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(GPIOE, &GPIO_InitStructure);                          //XFS,MP3,FM,GSM“Ù∆µ ‰≥ˆøÿ÷∆Ω≈	
+	
+	GPIO_ResetBits(GPIOC, GPIO_Pin_1);
+	GPIO_ResetBits(GPIOC, GPIO_Pin_2);
+	GPIO_ResetBits(GPIOC, GPIO_Pin_3);
+	GPIO_ResetBits(GPIOC, GPIO_Pin_4);
+	GPIO_ResetBits(GPIOC, GPIO_Pin_6);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_6;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOC, &GPIO_InitStructure);
 }
 
 
